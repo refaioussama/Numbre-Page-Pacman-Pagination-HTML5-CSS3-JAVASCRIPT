93a8900532c2d5c1cf41f07666b8de8cab07b172
refaioussama <42978000+refaioussama@users.noreply.github.com>
2018-09-12 18:29:38 +0100

    Numbre Page Pacman Pagination

---
+++ js/index.js
@@ -0,0 +1,15 @@
+const c = document.querySelector('.container')
+const indexs = Array.from(document.querySelectorAll('.index'))
+let cur = -1
+indexs.forEach((index, i) => {
+  index.addEventListener('click', (e) => {
+    c.className = 'container'
+    void c.offsetWidth;
+    c.classList.add('open')
+    c.classList.add(`i${i + 1}`)
+    if (cur > i) {
+      c.classList.add('flip')
+    }
+    cur = i
+  })
+})
